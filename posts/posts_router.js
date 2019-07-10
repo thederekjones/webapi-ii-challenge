ee1afa08999258e4e6811c1ea8f72da11d30ae6b
Derek Jones <derek@derekjones.io>
2019-07-10 02:33:21 -0400

    Add get() for /api/posts

--- posts/posts_router.js
+++ posts/posts_router.js
@@ -4,4 +4,16 @@ const Posts = require('./posts_model');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Posts.find();
+
+    res.status(200).json(posts);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
+});
+
 module.exports = router;
